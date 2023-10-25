3eceec7ab69fb654dd3d53db9f040689a2f893fe
Ganivada Mouli <vishnumouli0@gmail.com>
2023-10-25 18:02:54 +0530

    Basic server setup done

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const { PORT } = require("./config/serverConfig");
+// const apiRoutes = require("./routes/index");
+
+const db = require("./models/index");
+
+const setUpAndStartServer = async () => {
+  const app = express();
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  //   app.use("/api", apiRoutes);
+
+  app.listen(PORT, async () => {
+    console.log(`Server Started at Port: ${PORT}`);
+    if (process.env.DB_SYNC) {
+      db.sequelize.sync({ alter: true });
+    }
+  });
+};
+
+setUpAndStartServer();
